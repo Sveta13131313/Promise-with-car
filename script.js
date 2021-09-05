17599a8b0615bea0b673280f906d5a1fba8c07c5
Svetlana Khaydoshkina <sveta13131313@yandex.ru>
2021-09-05 15:07:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    select.addEventListener('change', () => {
+        const getData = () => {
+            return new Promise((resolve, reject) => {
+                const request = new XMLHttpRequest();
+                request.open('GET', './cars.json');
+                request.setRequestHeader('Content-type', 'application/json');
+              
+                request.addEventListener('readystatechange', () => {
+                   if (request.readyState !== 4){
+                       return;
+                   } 
+                    if(request.status === 200) {
+                        const response = JSON.parse(request.responseText);
+                        resolve(response);
+                    } else {
+                        reject();
+                    }
+                });
+                request.send();
+            });
+        
+        };
+        
+        
+        const outputData = (data) => {
+            data.cars.forEach(item => {
+                if (item.brand === select.value) {
+                    const { brand, model, price } = item;
+                    output.innerHTML = `Тачка ${brand} ${model} <br>
+                Цена: ${price}$`;
+                }
+            });
+        }
+        
+        
+        getData()
+            .then(outputData)
+            .catch(() => output.innerHTML =  'Произошла ошибка');
+    });
+
+});
+
